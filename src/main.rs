ebef8bd9b67d5e052bd3974f75ab7e71f1523423
TheBlckbird <theblckbrd@proton.me>
2023-11-11 10:12:33 +0100

    Add unit testing

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,100 @@
 #![no_main]
 #![no_std]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
+mod serial;
 mod vga_buffer;
 
 use core::panic::PanicInfo;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    // use core::fmt::Write;
-    // vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
-    // write!(
-    //     vga_buffer::WRITER.lock(),
-    //     ", some numbers: {} {}",
-    //     42,
-    //     1.337
-    // )
-    // .unwrap();
-
     println!("Hello World{}", "!");
     println!("Hello World{}", "!");
     println!("some numbers: {} {}", 42, 1.337);
+    serial_println!("some numbers: {} {}", 42, 1.337);
+
+    #[cfg(test)]
+    test_main();
 
     #[allow(clippy::empty_loop)]
     loop {}
 }
 
 /// This function is called on panic.
-
+#[cfg(not(test))]
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    println!("{}", _info);
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
+}
+
+// our panic handler in test mode
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
+    loop {}
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test_case]
+    fn trivial_assertion() {
+        // This asserts, testing works
+        let one = 1;
+        assert_eq!(1, one);
+        assert_ne!(1, 4);
+    }
+}
+
+pub trait Testable {
+    fn run(&self);
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    let tests_len = tests.len();
+    serial_println!(
+        "Running {} test{}",
+        tests_len,
+        if tests_len == 1 { "" } else { "s" }
+    );
+
+    for test in tests {
+        test.run();
+    }
+
+    exit_qemu(QemuExitCode::Success);
 }
