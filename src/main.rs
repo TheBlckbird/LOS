e46991a8c9e4e832c0e916d4f4fcb5f2fe9d72b0
TheBlckbird <theblckbrd@proton.me>
2023-11-11 08:43:52 +0100

    Add vga buffer writer

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,24 @@
 mod vga_buffer;
 
 use core::panic::PanicInfo;
-static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
+    // use core::fmt::Write;
+    // vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
+    // write!(
+    //     vga_buffer::WRITER.lock(),
+    //     ", some numbers: {} {}",
+    //     42,
+    //     1.337
+    // )
+    // .unwrap();
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xf0;
-        }
-    }
+    println!("Hello World{}", "!");
+    println!("Hello World{}", "!");
+    println!("some numbers: {} {}", 42, 1.337);
 
-    // #[allow(clippy::empty_loop)]
+    #[allow(clippy::empty_loop)]
     loop {}
 }
 
@@ -25,5 +29,6 @@ pub extern "C" fn _start() -> ! {
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    println!("{}", _info);
     loop {}
 }
