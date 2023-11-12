772cfcf6bb7caeddd4f04cf6cc8e3b7a97b8780d
TheBlckbird <theblckbrd@proton.me>
2023-11-12 16:21:21 +0100

    unit testing

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 #![no_main]
 #![no_std]
 #![feature(custom_test_frameworks)]
-#![test_runner(testing::test_runner)]
+#![test_runner(test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
 mod exit_qemu;
 mod serial;
-mod testing;
 mod vga_buffer;
 
 use core::panic::PanicInfo;
@@ -18,7 +17,7 @@ pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
     println!("Hello World{}", "!");
     println!("some numbers: {} {}", 42, 1.337);
-    serial_println!("some numbers: {} {}", 42, 1.337);
+    println!("Louis");
 
     #[cfg(test)]
     test_main();
@@ -51,4 +50,40 @@ tests! {
         assert_eq!(1, one);
         assert_ne!(1, 4);
     }
+}
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    let tests_len = tests.len();
+    serial_println!(
+        "Running {} test{}",
+        tests_len,
+        if tests_len == 1 { "" } else { "s" }
+    );
+
+    for test in tests {
+        test();
+    }
+
+    exit_qemu(QemuExitCode::Success);
+}
+
+// test suite helper
+#[macro_export]
+macro_rules! tests {
+    {$($name:ident $body:block)*} => {
+        $(
+            #[cfg(test)]
+            #[test_case]
+            fn $name() {
+                $crate::serial_print!("test ");
+                for word in stringify!($name).split('_') {
+                    $crate::serial_print!("{} ", word);
+                }
+                $crate::serial_print!("\t");
+                $body
+                $crate::serial_println!("[ok]");
+            }
+        )*
+    };
 }
